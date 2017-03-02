32ab433a0637d9c1c43c1623e30fab0c86c06ad5
Ivan Kipyegon <ivan@bardo.com>
2017-03-02 17:47:45 +0300

    [Chore] Add database migrations files

--- model.py
+++ model.py
@@ -15,7 +15,7 @@ class Bookmark(db.Model):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
+    email = db.Column(db.String(100), unique=True)
 
     def __repr__(self):
         return "<User {} >".format(self.username)
