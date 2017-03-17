024c83f58df598ebd4bab6cf8958f30c8442de9e
Ivan Kipyegon <ivan@bardo.com>
2017-03-17 12:52:42 +0300

    [Chore]Add API version, description and default error handlers

---
+++ BucketListAPI/api/restplus.py
@@ -0,0 +1,27 @@
+import logging
+import traceback
+import os
+
+from flask_restplus import Api
+from sqlalchemy.orm.exc import NoResultFound
+
+
+log = logging.getLogger(__name__)
+
+api = Api(version='1.0', title='BucketList Application API',
+          description='API for an online Bucket List service')
+
+
+@api.errorhandler
+def default_error_handler(e):
+    message = 'An unhandled exception occurred.'
+    log.exception(message)
+
+    if not os.environ['APP_SETTINGS']['DEBUG']:
+        return {'message': message}, 500
+
+
+@api.errorhandler(NoResultFound)
+def database_not_found_error_handler(e):
+    log.warning(traceback.format_exc())
+    return {'message': 'A database result was required but none was found.'}, 404
