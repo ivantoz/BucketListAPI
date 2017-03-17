2a5a14370a11fba04b11ebfea1332bd7f6bdc773
Ivan Kipyegon <ivan@bardo.com>
2017-03-17 12:59:50 +0300

    [Chore] Add model metadata objects for autogenerate support

--- migrations/env.py
+++ migrations/env.py
@@ -1,8 +1,10 @@
 from __future__ import with_statement
+
+import logging
+from logging.config import fileConfig
+
 from alembic import context
 from sqlalchemy import engine_from_config, pool
-from logging.config import fileConfig
-import logging
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -15,8 +17,8 @@ logger = logging.getLogger('alembic.env')
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from model import Result
-target_metadata = Result.metadata
+from BucketListAPI.database.model import Bucketlist, BucketListItems
+target_metadata = Bucketlist.metadata, BucketListItems.metadata
 from flask import current_app
 config.set_main_option('sqlalchemy.url',
                        current_app.config.get('SQLALCHEMY_DATABASE_URI'))
