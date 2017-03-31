964df2b4c45fa310ff5369c14e238e530a896912
Ivan Kipyegon <ivan@bardo.com>
2017-03-31 14:13:13 +0300

    [Feature] Add setup file for creating api as a package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='BucketListAPI',
+    packages=['BucketListAPI'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
