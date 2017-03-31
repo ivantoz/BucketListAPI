6e3e58ef44c8ff57c5268c491e34002d2e38d190
Ivan Kipyegon <ivan@bardo.com>
2017-03-31 14:11:13 +0300

    [Feature] Add database migration functions

--- manage.py
+++ manage.py
@@ -1,9 +1,10 @@
 import os
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
-from BucketListAPI.app import app
-from BucketListAPI.database import db
 
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+from BucketListAPI.app import app
+from BucketListAPI.model import db
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 
@@ -11,6 +12,7 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
 
 
 if __name__ == '__main__':
