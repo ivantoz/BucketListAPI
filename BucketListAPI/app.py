de92cfc6c415497de49fe7a39a1ad0f3996b9cf4
Ivan Kipyegon <ivan@bardo.com>
2017-03-31 14:06:20 +0300

    [Feature] Add flask app intialization, configuration and running functions

--- BucketListAPI/app.py
+++ BucketListAPI/app.py
@@ -4,7 +4,7 @@ import os
 from flask import Flask, Blueprint
 
 from BucketListAPI.api.bucketlists.endpoints.bucketlist import ns as bucketlist_namespace
-from BucketListAPI.api.bucketlists.endpoints.items import ns as bucketlist_item_namespace
+from BucketListAPI.api.auth.endpoints.register import ns as auth_new_user_namespace
 from BucketListAPI.api.restplus import api
 from BucketListAPI.model import db
 
@@ -23,8 +23,8 @@ def initialize_app(flask_app):
 
     blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
     api.init_app(blueprint)
+    api.add_namespace(auth_new_user_namespace)
     api.add_namespace(bucketlist_namespace)
-    api.add_namespace(bucketlist_item_namespace)
     flask_app.register_blueprint(blueprint)
     db.init_app(flask_app)
 
@@ -32,7 +32,7 @@ def initialize_app(flask_app):
 def main():
     initialize_app(app)
     log.info('>>>>> Starting development server at http://{}/api/v1/ <<<<<'.format(
-        app.config['SERVER_NAME']))
+        app.config['FLASK_SERVER_NAME']))
     app.run(debug=app.config['DEBUG'])
 
 
