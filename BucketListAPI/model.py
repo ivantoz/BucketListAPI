7b4da692c3101fd6c99ea8b95088a45d3a8c7c67
Ivan Kipyegon <ivan@bardo.com>
2017-04-08 22:17:37 +0300

    [Feature] Add database models for user, tokens and bucketlist items

--- BucketListAPI/model.py
+++ BucketListAPI/model.py
@@ -1,50 +1,98 @@
 import datetime
 from flask_sqlalchemy import SQLAlchemy
-
-from passlib.apps import custom_app_context as pwd_context
-from itsdangerous import (TimedJSONWebSignatureSerializer
-                          as Serializer, BadSignature, SignatureExpired)
+from flask import current_app
+import jwt
 
 
 db = SQLAlchemy()
 
 
 class User(db.Model):
-    __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(32), index=True)
-    password_hash = db.Column(db.String(120))
-    first_name = db.Column(db.String(32))
-    last_name = db.Column(db.String(32))
-    # buckets = db.relationship('BucketList', backref=db.backref('user',
-    #                                                            cascade='all, delete-orphan',
-    #                                                            single_parent=True), lazy='dynamic')
+    """ User Model for storing user related details """
+    __tablename__ = "users"
 
-    def hash_password(self, password):
-        self.password_hash = pwd_context.encrypt(password)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
+    bucketlists = db.relationship('Bucketlist', backref='users',cascade="all, delete, "
+                                                                        "delete-orphan",
+                                  lazy="joined")
 
-    def verify_password(self, password):
-        return pwd_context.verify(password, self.password_hash)
+    def __init__(self, email, password, admin=False):
+        from BucketListAPI.app import bcrypt
+        self.email = email
+        self.password = bcrypt.generate_password_hash(
+            password, current_app.config.get('BCRYPT_LOG_ROUNDS')
+        ).decode()
+        self.registered_on = datetime.datetime.now()
+        self.admin = admin
 
-    def generate_auth_token(self, expiration=600):
-        from BucketListAPI.app import app
-
-        s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
-        return s.dumps({'id': self.id})
+    def encode_auth_token(self, user_id):
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=120),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                current_app.config.get('SECRET_KEY'),
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
 
     @staticmethod
-    def verify_auth_token(token):
-        from BucketListAPI.app import app
-
-        s = Serializer(app.config['SECRET_KEY'])
+    def decode_auth_token(auth_token):
+        """
+        Decodes the auth token
+        :param auth_token:
+        :return: integer|string
+        """
         try:
-            data = s.loads(token)
-        except SignatureExpired:
-            return None    # valid token, but expired
-        except BadSignature:
-            return None    # invalid token
-        user = User.query.get(data['id'])
-        return user
+            payload = jwt.decode(auth_token, current_app.config.get('SECRET_KEY'))
+            is_blacklisted_token = BlacklistToken.check_blacklist(auth_token)
+            if is_blacklisted_token:
+                return 'Token blacklisted. Please log in again.'
+            else:
+                return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
+
+
+class BlacklistToken(db.Model):
+    """
+    Token Model for storing JWT tokens
+    """
+    __tablename__ = 'blacklist_tokens'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    token = db.Column(db.String(500), unique=True, nullable=False)
+    blacklisted_on = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, token):
+        self.token = token
+        self.blacklisted_on = datetime.datetime.now()
+
+    def __repr__(self):
+        return '<id: token: {}'.format(self.token)
+
+    @staticmethod
+    def check_blacklist(auth_token):
+        # check whether auth token has been blacklisted
+        res = BlacklistToken.query.filter_by(token=str(auth_token)).first()
+        if res:
+            return True
+        else:
+            return False
 
 
 class Bucketlist(db.Model):
@@ -52,13 +100,12 @@ class Bucketlist(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80))
-    items = db.relationship('BucketListItem', backref=db.backref('bucketlist',
-                                                                 cascade='all, delete-orphan',
-                                                                 single_parent=True),
+    items = db.relationship('BucketListItem', backref='bucketlist',
+                            cascade='all, delete, delete-orphan', single_parent=True,
                             lazy='dynamic')
     date_created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     date_modified = db.Column(db.DateTime, onupdate=datetime.datetime.utcnow)
-    created_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    created_by = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     def __init__(self, name, created_by):
         self.name = name
@@ -75,7 +122,8 @@ class BucketListItem(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
-    bucketlist_id = db.Column(db.Integer, db.ForeignKey('bucketlist.id'), nullable=False)
+    bucketlist_id = db.Column(db.Integer, db.ForeignKey('bucketlist.id', ondelete='CASCADE'),
+                              nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     date_modified = db.Column(db.DateTime, onupdate=datetime.datetime.utcnow)
     done = db.Column(db.Boolean)
