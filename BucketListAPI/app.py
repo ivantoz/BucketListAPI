021ff224878945be73ea74554aa95108d67b78fa
Ivan Kipyegon <ivan@bardo.com>
2017-04-08 22:15:27 +0300

    [Feature] Add flask app configuration, blueprint initilaization and app initialization

--- BucketListAPI/app.py
+++ BucketListAPI/app.py
@@ -1,17 +1,19 @@
 import logging.config
 import os
-
 from flask import Flask, Blueprint
-
 from BucketListAPI.api.bucketlists.endpoints.bucketlist import ns as bucketlist_namespace
-from BucketListAPI.api.auth.endpoints.register import ns as auth_new_user_namespace
+from BucketListAPI.api.auth.endpoints.user_profile import ns as auth_namespace
 from BucketListAPI.api.restplus import api
 from BucketListAPI.model import db
+from flask_mail import Mail
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
 logging.config.fileConfig('logging.conf')
 log = logging.getLogger(__name__)
+mail = Mail()
+bcrypt = Bcrypt(app)
 
 
 def configure_app(flask_app):
@@ -23,10 +25,12 @@ def initialize_app(flask_app):
 
     blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
     api.init_app(blueprint)
-    api.add_namespace(auth_new_user_namespace)
+    api.add_namespace(auth_namespace)
+
     api.add_namespace(bucketlist_namespace)
     flask_app.register_blueprint(blueprint)
     db.init_app(flask_app)
+    mail.init_app(flask_app)
 
 
 def main():
