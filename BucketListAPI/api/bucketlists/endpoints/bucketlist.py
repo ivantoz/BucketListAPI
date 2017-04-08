3bf8880e953c635fdbee84f87406c0a160596fae
Ivan Kipyegon <ivan@bardo.com>
2017-04-08 22:05:28 +0300

    [Feature] Add resources endpoint for bucketlist

--- BucketListAPI/api/bucketlists/endpoints/bucketlist.py
+++ BucketListAPI/api/bucketlists/endpoints/bucketlist.py
@@ -5,56 +5,77 @@ from flask_restplus import Resource
 
 from BucketListAPI.api.bucketlists.business import create_bucketlist, delete_bucketlist, \
     update_bucketlist
-from BucketListAPI.api.bucketlists.serializers import bucketlist, bucketlist_with_items
+from BucketListAPI.api.bucketlists.serializers import bucketlist, \
+    bucketlist_item, page_of_bucketlist, page_of_bucketlist_items
 from BucketListAPI.api.bucketlists.business import create_bucketlist_item, update_item, delete_item
-from BucketListAPI.api.bucketlists.serializers import bucketlist_item
 from BucketListAPI.api.restplus import api
-from BucketListAPI.model import Bucketlist, BucketListItem
-from flask_httpauth import HTTPBasicAuth
+from BucketListAPI.model import Bucketlist
+from BucketListAPI.api.bucketlists.parsers import pagination_arguments
+from BucketListAPI.api.restplus import auth_required
+
 
 log = logging.getLogger(__name__)
 
 ns = api.namespace('bucketlist', description='Operations related to Bucketlist')
 
-auth = HTTPBasicAuth()
-
 
 @ns.route('/')
 class BucketListCollection(Resource):
 
-    # @auth.login_required
-    @api.marshal_list_with(bucketlist_with_items)
+    @api.expect(pagination_arguments)
+    @api.marshal_with(page_of_bucketlist)
+    @auth_required
     def get(self):
         """
         List all the created bucket list.
         """
-        bucketlist = Bucketlist.query.all()
-        return bucketlist
+        args = pagination_arguments.parse_args(request)
+        page = args.get('page', 1)
+        per_page = args.get('per_page', 10)
+        bucketlist_query = Bucketlist.query
+        bucketlists = bucketlist_query.paginate(page, per_page, error_out=False)
+        return bucketlists
 
     @api.response(201, 'Bucketlist successfully created.')
     @api.expect(bucketlist, validate=True)
+    @auth_required
     def post(self):
         """
         Create a new bucket list.
         """
         data = request.json
-        create_bucketlist(data)
-        return None, 201
+        return create_bucketlist(data), 201
 
 
 @ns.route('/<int:id>')
+@api.param('id', 'Bucketlist ID')
 @api.response(404, 'Bucketlist Item not found.')
 class BucketList(Resource):
 
-    @api.marshal_with(bucketlist_with_items)
+    @api.expect(pagination_arguments)
+    @api.marshal_with(page_of_bucketlist_items)
+    @auth_required
     def get(self, id):
         """
         Get single bucket list.
         """
-        return Bucketlist.query.filter(Bucketlist.id == id).one()
+        args = pagination_arguments.parse_args(request)
+        page = args.get('page', 1)
+        per_page = args.get('per_page', 10)
+        bucketlist_query = Bucketlist.query.get_or_404(id)
+        bucketlist_items_query = Bucketlist.query.filter(Bucketlist.id == id)
+        bucketlist_items = bucketlist_items_query[0].items
+        bucketlist_items_paginated = bucketlist_items.paginate(page, per_page, error_out=False)
+        bucketlist_items_paginated.date_modified = bucketlist_query.date_modified
+        bucketlist_items_paginated.created_by = bucketlist_query.created_by
+        bucketlist_items_paginated.id = bucketlist_query.id
+        bucketlist_items_paginated.date_created = bucketlist_query.date_created
+        bucketlist_items_paginated.name = bucketlist_query.name
+        return bucketlist_items_paginated
 
     @api.expect(bucketlist)
     @api.response(204, 'Bucketlist item successfully updated.')
+    @auth_required
     def put(self, id):
         """
         Update this bucket list.
@@ -62,52 +83,58 @@ class BucketList(Resource):
         * Specify the ID of the bucketlist to modify in the request URL path.
         """
         data = request.json
-        update_bucketlist(id, data)
-        return None, 204
+        return update_bucketlist(id, data), 204
 
     @api.response(204, 'BucketList item successfully deleted.')
+    @auth_required
     def delete(self, id):
         """
         Delete this single bucket list.
 
         """
-        delete_bucketlist(id)
-        return None, 204
+
+        return delete_bucketlist(id), 204
 
 
 @ns.route('/<int:id>/item/')
+@api.param('id', 'Bucketlist ID')
 @api.response(201, 'Bucketlist item successfully created.')
 class BucketlistItemsCollection(Resource):
+
     @api.expect(bucketlist_item)
+    @auth_required
     def post(self, id):
         """
         Create a new item in bucket list.
         """
-        create_bucketlist_item(id, request.json)
-        return None, 201
+
+        return create_bucketlist_item(id, request.json), 201
 
 
 @ns.route('<int:id>/item/<int:item_id>')
+@api.param('item_id', 'Bucketlist Item ID')
+@api.param('id', 'Bucketlist ID')
 @api.response(404, 'Item not found.')
 class BucketlistItem(Resource):
 
     @api.expect(bucketlist_item)
     @api.response(204, 'Item successfully updated.')
+    @auth_required
     def put(self, id, item_id):
         """
         Update a bucket list item.
         """
         data = request.json
-        update_item(id, item_id, data)
-        return None, 204
+        return update_item(id, item_id, data), 204
 
     @api.response(204, 'Item successfully deleted.')
+    @auth_required
     def delete(self,id, item_id):
         """
         Delete an item in a bucket list.
         """
-        delete_item(item_id)
-        return None, 204
+
+        return delete_item(item_id), 204
 
 
 
