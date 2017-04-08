27a96e776230e5b6b9d1c11ef89a3bc8299a57d9
Ivan Kipyegon <ivan@bardo.com>
2017-04-08 22:11:34 +0300

    [Feature] Add api model serializer for bucketlist resources

--- BucketListAPI/api/bucketlists/serializers.py
+++ BucketListAPI/api/bucketlists/serializers.py
@@ -2,9 +2,10 @@ from flask_restplus import fields
 from BucketListAPI.api.restplus import api
 
 bucketlist_item = api.model('Bucketlist Item', {
-    'name': fields.String(required=True, description='Bucketlist item name'),
+    'name': fields.String(required=True, description='Bucketlist item name', example='Climb to  '
+                                                                                     'the Peak of Mt. Kenya'),
     'date_created': fields.DateTime,
-    'done': fields.Boolean(required=True)
+    'done': fields.Boolean(required=True, example=True)
 })
 
 bucketlist_item_output = api.inherit('Bucketlist item output', bucketlist_item, {
@@ -20,16 +21,13 @@ pagination = api.model('A page of results', {
     'total': fields.Integer(description='Total number of results'),
 })
 
-page_of_bucketlist_item = api.inherit('Page of bucketlis item', pagination, {
-    'items': fields.List(fields.Nested(bucketlist_item))
-})
 
 bucketlist = api.model('Bucketlist', {
-    'id': fields.Integer(readOnly=True, nullable=False, description='The unique identifier of a '
-                                                            'Bucketlist'),
-    'name': fields.String(required=True, description='Bucketlist name'),
+    'id': fields.Integer(readOnly=True, nullable=False, description='The unique identifier of a  '
+                                                                    'Bucketlist'),
+    'name': fields.String(required=True, description='Bucketlist name', example='Climb Mountain'),
     'date_created': fields.DateTime,
-    'created_by': fields.Integer(required=True)
+    'created_by': fields.Integer(required=True, example=1)
 
 })
 
@@ -38,3 +36,13 @@ bucketlist_with_items = api.inherit('Bucketlist with items', bucketlist, {
     'date_modified': fields.DateTime
 
 })
+
+page_of_bucketlist = api.inherit('Page of bucketlist', pagination, {
+    'items': fields.List(fields.Nested(bucketlist_with_items))
+})
+
+page_of_bucketlist_items = api.inherit('Page of bucketlist item', bucketlist,
+                                       pagination,
+                                       {'items': fields.List(fields.Nested(bucketlist_item_output))
+})
+
