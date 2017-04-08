d3b7d7e804b928d590a65a47890764af2d4c0cdf
Ivan Kipyegon <ivan@bardo.com>
2017-04-08 22:20:11 +0300

    [Feature] Add alembic migration commands for database migration

--- manage.py
+++ manage.py
@@ -8,11 +8,23 @@ from BucketListAPI.model import db
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 
+
 migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
+with app.app_context():
+    @manager.command
+    def create_db():
+        """Creates the db tables."""
+        db.create_all()
+
+
+    @manager.command
+    def drop_db():
+        """Drops the db tables."""
+        db.drop_all()
 
 
 if __name__ == '__main__':
