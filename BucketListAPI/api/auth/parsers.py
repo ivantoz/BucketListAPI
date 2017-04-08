0257bdead8e9e40b60eaff0419b89f50dc8db3f1
Ivan Kipyegon <ivan@bardo.com>
2017-04-08 22:29:06 +0300

    [Fetaure] Add parsers for authorisations headers

---
+++ BucketListAPI/api/auth/parsers.py
@@ -0,0 +1,5 @@
+from flask_restplus import reqparse
+
+authorization_arguments = reqparse.RequestParser()
+authorization_arguments.add_argument('Authorization', location='headers', type=str,
+                                     required=True, help='Bearer authorization token')
