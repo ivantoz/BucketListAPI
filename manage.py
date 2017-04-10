f48ab7b76a5e3c92f7a0905c6b6ffadf978adbc9
Ivan Kipyegon <ivan@bardo.com>
2017-04-10 12:08:18 +0300

    [Feature] Add command for running tests

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 import os
+import unittest
 
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
@@ -13,6 +14,34 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def test():
+    """Runs the unit tests without test coverage."""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+
+@manager.command
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
+        return 0
+    return 1
 
 with app.app_context():
     @manager.command
