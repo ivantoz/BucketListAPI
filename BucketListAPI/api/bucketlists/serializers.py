0f82e82f8ed743d3195e97fee260f3b9992dcb8d
Ivan Kipyegon <ivan@bardo.com>
2017-04-12 13:53:29 +0300

    [Chore] Add fix to bucketlist and bucketlist item models

--- BucketListAPI/api/bucketlists/serializers.py
+++ BucketListAPI/api/bucketlists/serializers.py
@@ -4,13 +4,13 @@ from BucketListAPI.api.restplus import api
 bucketlist_item = api.model('Bucketlist Item', {
     'name': fields.String(required=True, description='Bucketlist item name', example='Climb to  '
                                                                                      'the Peak of Mt. Kenya'),
-    'date_created': fields.DateTime,
     'done': fields.Boolean(required=True, example=True)
 })
 
 bucketlist_item_output = api.inherit('Bucketlist item output', bucketlist_item, {
     'id': fields.Integer(readOnly=True, description='The unique identifier of a Bucketlist item'),
-    'date_modified': fields.DateTime(description='date modified')
+    'date_modified': fields.DateTime(description='date modified'),
+    'date_created': fields.DateTime,
 
 })
 
@@ -21,12 +21,17 @@ pagination = api.model('A page of results', {
     'total': fields.Integer(description='Total number of results'),
 })
 
+bucketlist_input = api.model('Bucketlist', {
+    'name': fields.String(required=True, description='Bucketlist name', example='Climb Mountain')
+
+})
 
 bucketlist = api.model('Bucketlist', {
     'id': fields.Integer(readOnly=True, nullable=False, description='The unique identifier of a  '
                                                                     'Bucketlist'),
     'name': fields.String(required=True, description='Bucketlist name', example='Climb Mountain'),
     'date_created': fields.DateTime,
+    'date_modified': fields.DateTime,
     'created_by': fields.Integer(required=True, example=1)
 
 })
