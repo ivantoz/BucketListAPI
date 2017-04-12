e57acd52d3d633de2a3c012b8af73cefed5909ae
Ivan Kipyegon <ivan@bardo.com>
2017-04-12 16:08:32 +0300

    [Chore] Add remove manage.py command for test

--- manage.py
+++ manage.py
@@ -33,33 +33,34 @@ COV = coverage.coverage(
 COV.start()
 
 
-@manager.command
-def test():
-    """Runs the unit tests without test coverage."""
-    tests = unittest.TestLoader().discover('BucketListAPI/tests', pattern='test*.py')
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
-
-
-@manager.command
-def cov():
-    """Runs the unit tests with coverage."""
-    tests = unittest.TestLoader().discover('BucketListAPI/tests')
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        COV.stop()
-        COV.save()
-        print('Coverage Summary:')
-        COV.report()
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        covdir = os.path.join(basedir, 'tmp/coverage')
-        COV.html_report(directory=covdir)
-        print('HTML version: file://%s/index.html' % covdir)
-        COV.erase()
-        return 0
-    return 1
+# @manager.command
+# def test():
+#     """Runs the unit tests without test coverage."""
+#     # tests = unittest.TestLoader().discover('BucketListAPI/tests', pattern='test*.py')
+#     # result = unittest.TextTestRunner(verbosity=2).run(tests)
+#
+#     if result.wasSuccessful():
+#         return 0
+#     return 1
+#
+#
+# @manager.command
+# def cov():
+#     """Runs the unit tests with coverage."""
+#     tests = unittest.TestLoader().discover('BucketListAPI/tests')
+#     result = unittest.TextTestRunner(verbosity=2).run(tests)
+#     if result.wasSuccessful():
+#         COV.stop()
+#         COV.save()
+#         print('Coverage Summary:')
+#         COV.report()
+#         basedir = os.path.abspath(os.path.dirname(__file__))
+#         covdir = os.path.join(basedir, 'tmp/coverage')
+#         COV.html_report(directory=covdir)
+#         print('HTML version: file://%s/index.html' % covdir)
+#         COV.erase()
+#         return 0
+#     return 1
 
 
 @manager.command
