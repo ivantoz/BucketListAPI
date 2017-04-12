e97d291883519dbf83d0de7a3e2b8061c3ffac7c
Ivan Kipyegon <ivan@bardo.com>
2017-04-12 13:51:00 +0300

    [Feature] Add search argument for search by name functionality

--- BucketListAPI/api/bucketlists/parsers.py
+++ BucketListAPI/api/bucketlists/parsers.py
@@ -5,3 +5,6 @@ pagination_arguments.add_argument('page', type=int, required=False, default=1, h
 pagination_arguments.add_argument('bool', type=bool, required=False, default=1, help='Page number')
 pagination_arguments.add_argument('per_page', type=int, required=False, choices=[2, 10, 20, 30, 40, 50],
                                   default=10, help='Results per page {error_msg}')
+
+search_argument = reqparse.RequestParser()
+search_argument.add_argument('q', type=str, required=False, help='name to search')
